0a3ccb32be48287f89b94691623618447b574c6a
seanmai <seandmai@gmail.com>
2017-11-18 15:41:54 -0700

    Added passport config for new User model

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
+var bcrtyp = require("bcrypt-nodejs");
 
 var UserSchema = new mongoose.Schema({
     email: {type: String, required: true},
@@ -8,6 +9,14 @@ var UserSchema = new mongoose.Schema({
         first: {type: String, required: true},
         last:  {type: String, required: true}}
 });
+
+UserSchema.methods.encrypPassword = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(5), null);
+};
+
+UserSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 
 UserSchema.plugin(passportLocalMongoose);
 module.exports = mongoose.model("User", UserSchema);
