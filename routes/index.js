bb98a7a3e72986cf3e1ca39c0f79809c9fd664b3
seanmai <seandmai@gmail.com>
2017-11-18 14:31:01 -0700

    Added user login

--- routes/index.js
+++ routes/index.js
@@ -29,5 +29,16 @@ router.post("/register", function(req, res){
     })
 });
 
+//Show login form
+router.get("/login", function(req, res){
+    res.render("login", {page: "login"});
+});
+
+//Handles login logic using passport middleware
+router.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+    }), function(req, res){
+});
 
 module.exports = router;
