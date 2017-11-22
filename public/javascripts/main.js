40ed2c5e12bbbb5d19add13b762f7530249e30a2
seanmai <seandmai@gmail.com>
2017-11-21 23:34:28 -0700

    Added category navbar to index and adjusted styling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,26 @@
-$("select")function(){
-    var $select = $(".qty");
-    for (i=1;i<=10;i++){
-        $select.append($('<option></option>').val(i).html(i))
-    }
-});​
+// $("select")function(){
+//     var $select = $(".qty");
+//     for (i=1;i<=10;i++){
+//         $select.append($('<option></option>').val(i).html(i))
+//     }
+// });​
+
+// js for sticking category navbar
+$(document).ready(function(){
+    var navOffset = ($(".navbar-stick").offset().top)-($(".navbar-header").outerHeight());
+    alert(navOffset);
+
+    $(".navbar-stick").wrap('<div class="nav-placeholder"></div>');
+    $(".nav-placeholder").height($(".navbar-stick").outerHeight());
+
+    $(window).scroll(function(){
+        var scrollPos = $(window).scrollTop();
+
+         if(scrollPos >= navOffset){
+             $(".navbar-stick").addClass("navbar-fixed-top navbar-fixed");
+         } else {
+             $(".navbar-stick").removeClass(" navbar-fixed-top navbar-fixed");
+         }
+
+    });
+});
