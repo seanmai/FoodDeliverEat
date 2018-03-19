0abe39ac11416b639e20d0f3ed95e7745d903a68
seanmai <seandmai@gmail.com>
2018-03-19 16:41:18 -0600

    Added slideDown and slideUp animation on dropdown

--- public/javascripts/dropdown.js
+++ public/javascripts/dropdown.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+    //Resolve conflict when adding AJAX link
     $(".dropdown-toggle").dropdown();
+
+    // Add slideDown animation to Bootstrap dropdown when expanding.
+    $('.dropdown').on('show.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+
+    // Add slideUp animation to Bootstrap dropdown when collapsing.
+    $('.dropdown').on('hide.bs.dropdown', function() {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
 });
